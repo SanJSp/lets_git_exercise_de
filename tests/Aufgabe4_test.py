0a403ad246f710e35266ad4db9fd696adcb4bd4a
Sandro Sp <38314662+SanJSp@users.noreply.github.com>
2020-06-22 13:47:03 +0200

    Update Aufgabe4_test.py

--- tests/Aufgabe4_test.py
+++ tests/Aufgabe4_test.py
@@ -12,7 +12,7 @@ def check_if_string_in_file(file_name, string_to_search):
 	Lines = f.readlines()
 	regEx = string_to_search + '\s?([A-Z]|[a-z]|[\u00c4-\u02AF]|[0-9])+'
 	for line in Lines:
-		if re.findall('>\s?("|'|[A-Z]|[a-z]|[\u00c4-\u02AF]|[0-9])+', line):
+		if re.findall('>\s?([A-Z]|[a-z]|[\u00c4-\u02AF]|[0-9])+', line):
 			return True
 	f.close()
 	return False
